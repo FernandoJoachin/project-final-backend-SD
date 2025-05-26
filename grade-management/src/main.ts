a99e4a829c914fdbb306106b3ba88e40c9c107db
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2025-05-26 00:18:53 -0600

    Update main.ts

--- grade-management/src/main.ts
+++ grade-management/src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONT_URL, 
+    origin: true, 
     methods: 'GET,POST,PATCH,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
